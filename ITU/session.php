8196115d644e00e65cea853160521628e46cbfd3
nikitau <102725225+nikitauty@users.noreply.github.com>
2023-04-06 21:14:23 +0300

    Add files via upload

--- ITU/session.php
+++ ITU/session.php
@@ -1,4 +1,13 @@
 <?php
 session_start();
-$_SESSION['name'] = 'Nik';
-echo $_SESSION['name'];
+error_reporting(0);
+
+$login = $_GET['login'];
+$pass = $_GET['pass'];
+if ($login == "lol" AND $pass == "1234"){
+    $_SESSION['login'] = $login;
+    header('Location: profile.php');
+} else {
+    echo "<h1>Введите корректные данные</h1>";
+}
+
