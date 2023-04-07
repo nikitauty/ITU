ef566ecdcc997126726be6722da7a3bca4e74c05
nikitau <102725225+nikitauty@users.noreply.github.com>
2023-04-07 23:08:47 +0300

    Add files via upload

--- ITU/session/session.php
+++ ITU/session/session.php
@@ -1,32 +1,56 @@
 <?php
 session_start();
 error_reporting(0);
+function isAuth() { //Проверка на авторизованного пользователя
+    return $_SESSION["is_auth"] ?? false;
+}
 
-$err = '';
-$login = $_POST['email'];
+function auth($login, $pass) : bool { //Проверка пароля
+    if ($login === "lol" AND $pass === "1234"){
+        $_SESSION["is_auth"] = true;
+        $_SESSION["login"] = $login;
+        return true;
+    } else {
+        $_SESSION["is_auth"] = false;
+        return false;
+    }
+}
+
+$login = $_POST['name'];
 $pass = $_POST['pass'];
-if ($login == "lol" AND $pass == "1234"){
-    $_SESSION['login'] = $login;
-    header('Location: profile.php');
-} else {
-    $err = "введите корректные данные";
+
+$err = "Введите корректные данные";
+
+$form = '<form action="session.php" method="post">
+    <p>Email: <input type="text" name="name" /></p>
+    <p>Password: <input type="password" name="pass" /></p>
+    <p><input type="submit" value="Войти"/></p>
+</form>';  //Шаблон формы
+
+if ($_POST['delete']){ //Удаление сессии
+    $_SESSION = [];
+    session_destroy();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-
 </head>
 <body>
-<form action="session.php" method="post">
-    <p>Email: <input type="text" name="email" /></p>
-    <p>Password: <input type="text" name="pass" /></p>
-    <p><input type="submit" /></p>
-</form>
-
-<?=$err?>
-
+<?php if (isset($_POST['name']) AND isset($_POST['pass']) AND !auth($login, $pass)):?>
+    <?=$err?>
+<?php endif; ?>
+<?php if(isAuth()): ?>
+    <h1>Авторизация прошла успешно</h1>
+    Username: <?=$_SESSION['login']?> <br/>
+    <form method="post">
+        <p><input type="submit" name="delete" value="Выйти"/></p>
+    </form>
+<?php else: ?>
+    <?=$form?>
+<?php endif; ?>
 </body>
 </html>
